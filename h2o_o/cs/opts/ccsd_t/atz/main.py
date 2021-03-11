7541fb2ea36ac6df354f6c13e94b0542f3903389
Stephen Merritt Goodlett <smg13363@ss-sub1.gacrc.uga.edu>
2021-03-11 13:04:53 -0500

    update

---
+++ h2o_o/cs/opts/ccsd_t/atz/main.py
@@ -0,0 +1,14 @@
+import optavc
+options = { 
+    'program': 'psi4',
+    'template_file_path': 'template.dat',
+    'energy_regex': r'Stephenergy (-\d*.\d*)',
+    'time_limit': '6-20:00:00',
+    'queue': 'batch',
+    'max_force_g_convergence': 1e-7,
+    'nslots': '12',
+    'memory': '5GB',
+    'name':'h2o_o_cs'
+}
+optavc.run_optavc('OPT', options)
+
